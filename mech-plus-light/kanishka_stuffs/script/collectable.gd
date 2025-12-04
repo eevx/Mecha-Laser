1da472c16878ebb3700ad7ee26277ce12cbc8b74
Kanishka <kanishkamahale18@gmail.com>
2025-12-04 20:49:07 +0530

    collectable

--- mech-plus-light/kanishka_stuffs/script/collectable.gd
+++ mech-plus-light/kanishka_stuffs/script/collectable.gd
@@ -2,11 +2,13 @@ extends Area2D
 @onready var collectable: Sprite2D = $Coin
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var pickup: AudioStreamPlayer2D = $pickup
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 var coinscore=0
 func _on_body_entered(_body) -> void:
 	coinscore=coinscore+1
 	collectable.visible=false
-	collision_shape_2d.disabled=true
-	pickup.playing=true
+
+
 	print ("your collected a coin")
+	animation_player.play("new_animation")
