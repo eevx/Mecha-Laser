2c3532cf7f417711b3368a4c631a8e3fe0eeaf94
eevx <155942309+eevx@users.noreply.github.com>
2025-12-06 12:50:00 +0530

    s

--- mech-plus-light/Scripts/Customs/FSM_V1/thruster_state.gd
+++ mech-plus-light/Scripts/Customs/FSM_V1/thruster_state.gd
@@ -1,4 +1,5 @@
 extends Player_State
+@onready var thruster_ui: Line2D = $"../Thruster_UI"
 
 func Enter() -> void:
 	if not player.thruster_fuel > 0.0:
@@ -11,6 +12,7 @@ func Physics_Update(delta: float) -> void:
 	if pressing and player.thruster_fuel > 0.0:
 		apply_thruster_force(delta)
 		player.thruster_fuel = max(player.thruster_fuel - player.data.thruster_drain_rate * delta, 0.0)
+		thruster_ui.points[1] = Vector2(0,player.thruster_fuel)
 		player.thruster_refill_timer = 0.0
 		if player.thruster_fuel <= 0.0:
 			stop_thruster_effects()
