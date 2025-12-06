5fb0e337a56c7bcbe265823f24e6b5938cdd043e
Aditya Vishwakarma <adityav2707@gmail.com>
2025-12-06 15:54:11 +0530

    DONE WITH ANIMATION
    light animation not included

--- mech-plus-light/Scripts/Customs/FSM_V1/idle_state.gd
+++ mech-plus-light/Scripts/Customs/FSM_V1/idle_state.gd
@@ -1,6 +1,7 @@
 extends Player_State
 
 func Enter():
+	# Play idle animation when entering idle state
 	player.PlayerSprite.play("idle")
 	player.jumpCount = 1
 	player.dashCount = 1
@@ -8,21 +9,25 @@ func Enter():
 func Physics_Update(_delta:float):
 	var dir := Input.get_axis("left","right")
 	player._apply_gravity()
+	
 	if not player.is_on_floor():
 		Transition("AirState")
 		return
-
+	
 	if dir != 0:
 		Transition("RunState")
 		return
-
+	
 	if Input.is_action_pressed("jump") and player.jumpCount > 0:
 		Transition("AirState")
-
-	if Input.is_action_pressed("dash") and player.dashCount > 0:
+	
+	# Only allow dash if cooldown is ready
+	if Input.is_action_pressed("dash") and player.dashCount > 0 and get_parent().get_node("DashState").dashcool:
 		Transition("DashState")
 	
-	if Input.is_action_just_pressed("thruster") and player.thruster_fuel > 0.0:
+	# Check if player has minimum fuel to start thruster (using is_action_pressed for held input)
+	if Input.is_action_pressed("thruster") and player.thruster_fuel >= player.data.thruster_min_fuel_to_start:
 		Transition("ThrusterState")
+
 func Exit():
 	pass
